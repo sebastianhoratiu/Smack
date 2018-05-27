6c3c9942e732e14efbed03ca262cd2a3fcbf88e0
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-05-27 10:28:34 +0300

    End of lesson 4; UI and navigation to Login View Controller created as well as closing it.

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Sebastian Horatiu on 27/05/2018.
+//  Copyright © 2018 Sebastian Horatiu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
