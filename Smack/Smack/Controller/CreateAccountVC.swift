6b6bf0429b67ae902ec8e6e9c3c4c5a68b184929
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-07-06 08:49:31 +0300

    Fixed encoding in the Alamofire.request method from URLEncoding to JSONEncoding.

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //MARK: Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view. 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let pass = passTxt.text, passTxt.text != "" else { return }
+        
+        print("Calling registerUser with the following parameters: email = \(email), password = \(pass)")
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            } else {
+                print("not registered user - error")
+            }
+        }
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
     @IBAction func closeBtnPressed(sender: Any) {
--- Smack/Smack/Services/AuthService.swift
+++ Smack/Smack/Services/AuthService.swift
@@ -43,20 +43,17 @@ class AuthService {
     
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         let lowerCaseEmail = email.lowercased()
-        let header = ["Content-Type":"application/json; charset=utf-8"]
+        let header = ["Content-Type": "application/json; charset=utf-8"]
         let body: [String: Any] = ["email": lowerCaseEmail,
                                    "password": password]
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: URLEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
+                print("Url: \(URL_REGISTER), parameters: \(body), headers: \(header)")
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
     }
-    
-    
-    
-    
 }
