4daf648289f207e7315cb643a6dceec8265a2801
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-07-08 14:33:50 +0300

    End of lesson 11. Now logging in the user we've created in the previous lesson.

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -34,6 +34,11 @@ class CreateAccountVC: UIViewController {
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
                 print("registered user")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("Logged in user!", "AuthToken: \(AuthService.instance.authToken)")
+                    }
+                })
             } else {
                 print("not registered user - error")
             }
