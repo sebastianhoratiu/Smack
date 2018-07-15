7607a965bc22731a643c8bc5ca0306a4c1e538e9
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-07-15 12:28:57 +0300

    End of lesson-12 Creating a user. Now also creating a user and dismissing the CreateUserVC afterwards.

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,10 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    //MARK: Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +30,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccntPressed(_ sender: Any) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text, emailTxt.text != "" else { return }
         guard let pass = passTxt.text, passTxt.text != "" else { return }
         
@@ -37,6 +42,17 @@ class CreateAccountVC: UIViewController {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
                         print("Logged in user!", "AuthToken: \(AuthService.instance.authToken)")
+                        print("Calling createUser with the following parameters: \(name, email, self.avatarName, self.avatarColor)")
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print("createUser - Success!")
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
+                            } else {
+                                print("createUser ran into some error?")
+                                print("The description of success is: \(success.description)")
+                            }
+                        })
                     }
                 })
             } else {
