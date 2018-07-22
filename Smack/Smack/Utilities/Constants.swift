13e354f17272c542e7afa8dd2b7485c13860aa24
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-07-22 10:59:27 +0300

    Lesson-13 Created the Avatar Picket VC, Avatar Cell model, for now with just gray cells and just the back button dismissing the VC. No action implemented yet for the segmented control, nor for the cells.

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -20,6 +20,7 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+let TO_AVATAR_PICKER = "toAvatarPicker"
 
 //MARK: User Defaults
 let TOKEN_KEY = "token"
