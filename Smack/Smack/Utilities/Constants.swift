01c9cfbb02356741df0fbbc26790330d9095f79d
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-08-28 08:31:18 +0300

    Lesson 18: Created the find user by email function. Extracted a few constants and a function so that we're not repeating code. Some re-indentation of code.

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://slackchatcloneseb.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)//user/byEmail/"
 
 //MARK: Segues
 let TO_LOGIN = "toLogin"
@@ -29,6 +30,10 @@ let USER_EMAIL = "userEmail"
 
 //MARK: Headers
 let HEADER = ["Content-Type": "application/json; charset=utf-8"]
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
+]
 
 //MARK: Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
