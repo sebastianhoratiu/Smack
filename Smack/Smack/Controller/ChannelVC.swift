d379fff35373b4dbc4088301618f242cceb45f40
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-10-16 08:08:08 +0300

    Lesson 23: modified the addChannelPressed method to only present the addChannel VC if user is logged in.

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -40,10 +40,11 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     @IBAction func addChannelPressed(_ sender: Any) {
-        let addChannel = AddChannelVC()
-        addChannel.modalPresentationStyle = .custom
-        present(addChannel, animated: true, completion: nil)
-        
+        if AuthService.instance.isLoggedIn {
+            let addChannel = AddChannelVC()
+            addChannel.modalPresentationStyle = .custom
+            present(addChannel, animated: true, completion: nil)
+        }
     }
 
     @IBAction func loginBtnPressed (_ sender: Any) {
