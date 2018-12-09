05b9466b805d831b185ca9725801e97531c9d44d
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2018-12-09 13:41:47 +0200

    Now getting a message automatically after it was posted, by using another on() method that listens to the messageCreated event. Also now scrolling the tableView right down to the last message.

--- Smack/Smack/Controller/ChatVC.swift
+++ Smack/Smack/Controller/ChatVC.swift
@@ -39,6 +39,14 @@ class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNEL_SELECTED, object: nil)
         
+        SocketService.instance.getChatMessage { (success) in
+            if success {
+                self.tableView.reloadData()
+                let endIndex = IndexPath(row: MessageService.instance.messages.count - 1 , section: 0)
+                self.tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+            }
+        }
+        
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail { (success) in
                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
