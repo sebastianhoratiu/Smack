828b46fc816470a6a51f836a228c48e7eac4c7e2
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2019-03-05 08:32:11 +0200

    update-profile Now pre-populating the fields in UpdateProfileVC with the current values.

--- Smack/Smack/Controller/UpdateProfileVC.swift
+++ Smack/Smack/Controller/UpdateProfileVC.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class UpdateProfileVC: UIViewController {
+    
+    //MARK: Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
+    //MARK: Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    var bgColor: UIColor?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupView()
     }
 
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    func setupView() {
+        usernameTxt.text = UserDataService.instance.name
+        emailTxt.text = UserDataService.instance.email
+        userImg.image = UIImage(named: UserDataService.instance.avatarName)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+
+    }
 
 }
